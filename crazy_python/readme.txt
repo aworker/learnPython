d0cf7fddddf2132c98316c5d66d1d2160ed51139
limingyuan001 <limingyuan001@ke.com>
2018-07-24 14:10:00 +0800

    疯狂python提交记录

---
+++ crazy_python/readme.txt
@@ -0,0 +1,4 @@
+网易云课堂上的python学习课程
+http://study.163.com/course/courseLearn.htm?courseId=302001#/learn/video?lessonId=1048115233&courseId=302001
+
+疯狂的python
---
+++ crazy_python/unit_1/huatu.py
@@ -0,0 +1,15 @@
+import turtle
+
+#隐藏箭头
+turtle.hideturtle()
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+
+input()
